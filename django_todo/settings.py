2d43a8ead4ce4f3e5b825f83f1c87f9d6c4c4ae7
leah-jala <leahlafond@protonmail.com>
2022-12-05 12:32:53 +0000

    Set up environment variables and automatic deployment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ci-ljl-todo-app.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 DISABLE_COLLECTSTATIC = True
 # Application definition
